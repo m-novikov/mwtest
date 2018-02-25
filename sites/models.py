f74eb76b58edde8dc970b645808fb9844ff70ac0
Maxim Novikov <mnovikov.work@gmail.com>
2018-02-25 21:04:15 +0100

    Added date to Records

--- sites/models.py
+++ sites/models.py
@@ -13,5 +13,9 @@ class Record(models.Model):
         Site, verbose_name='Site', related_name='records',
         on_delete=models.CASCADE
     )
+    date = models.DateField(verbose_name='Date')
     a_value = models.FloatField(verbose_name='A Value')
     b_value = models.FloatField(verbose_name='B Value')
+
+    def __str__(self):
+        return '%s %s' % (self.site, self.date)
